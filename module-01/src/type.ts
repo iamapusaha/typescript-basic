d82871b9d90ccf332ee50b67301b2dc564b16ee9
iamapusaha <apusaha392@gmail.com>
2025-01-02 14:19:40 +0600

    add basic type

---
+++ module-01/src/type.ts
@@ -0,0 +1,11 @@
+let herName: string = "apple";
+let age: number = 78;
+let result: boolean = true;
+let hope: undefined = undefined;
+let luck: null = null;
+
+let studnetRoll: number[] = [10, 54, 45];
+
+let people: string[] = ["akash", "batas", "dula", "bali"];
+
+let ageName: [number, string] = [12, "nila"];
