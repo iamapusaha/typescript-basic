c9a7294c2f3736077c6d0905c009215e28401d2e
iamapusaha <apusaha392@gmail.com>
2025-01-02 14:38:04 +0600

    added reference type

---
+++ module-01/src/referenceType.ts
@@ -0,0 +1,15 @@
+const user: {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  age: number;
+  salary: number;
+  company: "dashIt";
+} = {
+  firstName: "Apu Saha",
+  middleName: "kumer",
+  lastName: "saha",
+  age: 28,
+  salary: 85,
+  company: "dashIt",
+};
