0cd626e7ba86c30540e385967147f6d9b0b71e78
iamapusaha <apusaha392@gmail.com>
2025-01-02 16:26:23 +0600

    rest operator

---
+++ module-01/dist/rest-operator.js
@@ -0,0 +1,5 @@
+"use strict";
+function greedingFriends(...friend) {
+    return `hi my all friends ${friend}`;
+}
+console.log(greedingFriends("akash", "batas", "megh", "katla", "argho"));
---
+++ module-01/src/rest-operator.ts
@@ -0,0 +1,5 @@
+function greedingFriends(...friend: string[]): string {
+  return `hi my all friends ${friend}`;
+}
+
+console.log(greedingFriends("akash", "batas", "megh", "katla", "argho"));
