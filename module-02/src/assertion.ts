174bc587cac81049503b8f0261666f0a8f72b133
iamapusaha <apusaha392@gmail.com>
2025-01-03 13:29:11 +0600

    type assertion

---
+++ module-02/src/assertion.ts
@@ -0,0 +1,18 @@
+{
+  let anythings: any;
+  anythings = "apni valo manus";
+  anythings = 255;
+
+  //   (anythings as number).toExponential
+  const kgToGm = (value: string | number) => {
+    if (typeof value === "string") {
+      const convertedValue = parseFloat(value) * 1000;
+      return `the converted number is ${convertedValue}`;
+    }
+    if (typeof value === "number") {
+      return value * 1000;
+    }
+  };
+
+  const result1 = kgToGm(1000) as number;
+}
